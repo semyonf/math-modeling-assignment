e4146c8b0277403542d205c1a71460f6c72ec6dc
Semyon Fomin <taxnuke@gmail.com>
2018-01-31 14:40:20 +0300

    прикручивает кривую оптимизацию

--- calc.js
+++ calc.js
@@ -1,26 +1,43 @@
 #!/usr/bin/env node
 const fs = require('fs');
 
-createCSV('data/theor.csv', calcTheorValues());
-createCSV('data/noised.csv', calcNoisedValues(calcTheorValues(), 0.05));
+const
+  theorValues = calcTheor(),
+  noisedValues005 = calcNoised(theorValues, 0.05);
+
+createCSV('data/theor.csv', theorValues);
+createCSV('data/noised.csv', noisedValues005);
+// createCSV('data/optimized.csv', calcTheor(1.481, 5.7368));
+// createCSV('data/optimized.csv', calcTheor(1.281, 3.9368));
+// createCSV('data/optimized.csv', calcTheor(2.0593, 4.2006));
+
+// optimize();
+// process.exit(1);
 
 ///////////////////////////////////////
 // ВЫЧИСЛЕНИЕ ТЕОРЕТИЧЕСКИХ ЗНАЧЕНИЙ //
 ///////////////////////////////////////
 
-function calcTheorValues() {
-  const xt = 5, k = 4, a = 3, b1 = 1, b2 = 2;
+function calcTheor(b1 = 1, k = 4, addedNoise = null) {
+  const XT = 5, A = 3, B2 = 2, B1 = b1, K = k;
   let y = 0, z1 = 0, z2 = 0, z3 = 0, h = 0.05, theorValues = [];
 
   for (let iterations = 1; iterations <= 500; iterations++) {
-    z3 = (xt - z1 - (b1 + a) * z2 - (b1 + a * b1) * z3) / (a * b2);
+    z3 = (XT - z1 - (B1 + A) * z2 - (B1 + A * b1) * z3) / (A * B2);
     z2 = z2 + h * z3;
     z1 = z1 + h * z2;
 
-    y = k * (z1 - a * z2);
+    y = K * (z1 - A * z2);
 
     if (iterations % 20 == 0) {
       theorValues.push(y);
+    }
+  }
+
+  if (addedNoise) {
+    let res = 0;
+    for (let i = 0; i < theorValues.length; ++i) {
+      res += Math.pow(theorValues[i] - addedNoise[i], 2);
     }
   }
 
@@ -36,7 +53,7 @@ function createCSV(filename, data, headers = 'x, y') {
 
   fs.writeFile(filename, output, (err) => {
     if (err) {
-      return console.log(err);
+      return console.error(err);
     }
 
     console.info(`${filename} saved!`);
@@ -47,13 +64,63 @@ function createCSV(filename, data, headers = 'x, y') {
 // ЗАШУМЛЕНИЕ //
 ////////////////
 
-function calcNoisedValues(theorValues, factor) {
+function calcNoised(theorValues, factor) {
   const deltaY = Math.max.apply(null, theorValues) * factor;
-  let noisedValues = theorValues;
+  let noisedValues = theorValues.slice();
 
   for (let i = 0; i < noisedValues.length; ++i) {
     noisedValues[i] += Math.random() * (2 * deltaY) - deltaY;
   }
 
   return noisedValues;
+}
+
+/////////////////
+// ОПТИМИЗАЦИЯ //
+/////////////////
+
+function optimize() {
+  let b1 = 2, k = 5, f, f1, f2, h1 = 0.1, h2 = 0.1;
+
+  const noisedValues = calcNoised(calcTheor(), 0.05);
+
+  let m = 1;
+
+  f = calcTheor(b1, k, noisedValues);
+  f2 = f;
+
+  while (true) {
+    if (m == 1) {
+      b1 = b1 + h1;
+      f1 = calcTheor(b1, k, noisedValues); // insert function
+      if (f1 <= f) {
+          h1 = 3 * h1;
+          f = f1;
+      } else {
+          b1 = b1 - h1;
+          h1 = -0.5 * h1;
+      }
+      ++m;
+    } else if (m == 2) {
+      k = k + h2;
+      f1 = calcTheor(b1, k, noisedValues); // insert function
+      if (f1 <= f) {
+          h2 = 3 * h2;
+          f = f1;
+      } else {
+          k = k - h2;
+          h2 = -0.5 * h2;
+      }
+      m = 1;
+    }
+
+    console.log(b1, k);
+
+    if (Math.abs(f2 - f1) < 0.05) {
+      break;
+      process.exit(1);
+    }
+
+    f2 = f1;
+  }
 }
