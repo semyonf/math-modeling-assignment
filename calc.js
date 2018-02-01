33311c8329c154582d27dfdc779658d46f8d62f4
Semyon Fomin <taxnuke@gmail.com>
2018-02-01 19:16:05 +0300

    рефакторит скрипт

--- calc.js
+++ calc.js
@@ -1,173 +1,167 @@
 #!/usr/bin/env node
-const fs = require('fs');
+;(function (undefined) {
+  'use strict';
 
-const
-  theoreticalValues     = getFunctionValues(),
-  experimentalValues005 = getExperimental(theoreticalValues, 0.05),
-  experimentalValues01 = getExperimental(theoreticalValues, 0.1),
-  experimentalValues02 = getExperimental(theoreticalValues, 0.2),
-  optimized = optimize(function(b1, k) {
-    let experimentalValues = experimentalValues02.slice();
-    const modelValues = getFunctionValues(b1, k);
+  const fs = require('fs');
 
-    let value = 0;
-    for (let i = 0; i < modelValues.length; ++i) {
-      value += Math.pow(experimentalValues[i] - modelValues[i], 2);
-    }
+  const
+    theoreticalValues = getModel(),
 
-    return value;
-  }, 2, 5);
-  // optimizedEllipse = optimize(function(x, y) {
+    experimental1 = getExperimental(theoreticalValues, 0.05),
+    experimental2  = getExperimental(theoreticalValues, 0.1),
+    experimental3  = getExperimental(theoreticalValues, 0.2);
+
+  /**
+   * CF
+   */
+  optimize(function(b1, k) {
+    const
+      modelValues = getModel(b1, k),
+      experimentalValues = experimental1;
+
+    const cf = experimentalValues.reduce(function(sum, current, i) {
+      return sum + Math.pow(current - modelValues[i], 2);
+    }, 0);
+
+    return cf;
+  }, 2, 5, (b1, k, steps) => {
+    createCSV(
+      'data/optimized.csv',
+      getModel(b1, k)
+    );
+
+    console.info(steps);
+  });
+
+  /**
+   * Ellipse (precomputed)
+   */
+  // optimize(function(x, y) {
   //   return Math.sqrt(x*x + y*y);
-  // }, 4, 3),
-  // optimizedRosenbrock = optimize(function(x, y) {
+  // }, 4, 3, function (x, y) {});
+
+  /**
+   * Rosenbrock (precomputed)
+   */
+  // optimize(function(x, y) {
   //   return ((1-x)*(1-x)) + 100 * ((y - x*x) * (y - x*x));
-  // }, -5, -3);
+  // }, 5, 3, function (x, y) {});
 
-createCSV(
-  'data/theor.csv',
-  theoreticalValues
-);
+  createCSV('data/theor.csv', theoreticalValues);
+  createCSV('data/noised005.csv', experimental1);
+  createCSV('data/noised01.csv', experimental2);
+  createCSV('data/noised02.csv', experimental3);
+  createCSV('data/final.csv', getModel(0.973, 3.984));
+  createCSV('data/rnd.csv', checkRandomDistribution(), 'intrval, occurences');
 
-createCSV(
-  'data/noised005.csv',
-  experimentalValues005
-);
+  function getModel(b1 = 1, k = 4) {
+    const XT = 5, A = 3, B2 = 2, B1 = b1, K = k;
+    let y = 0, z1 = 0, z2 = 0, z3 = 0, h = 0.05, theorValues = [];
 
-createCSV(
-  'data/noised01.csv',
-  experimentalValues01
-);
+    for (let i = 1; i <= 500; ++i) {
+      z3 = (XT - z1 - (B1 + A) * z2 - (B1 + A * b1) * z3) / (A * B2);
+      z2 = z2 + h * z3;
+      z1 = z1 + h * z2;
 
-createCSV(
-  'data/noised02.csv',
-  experimentalValues02
-);
+      y = K * (z1 - A * z2);
 
-createCSV(
-  'data/optimized.csv',
-  getFunctionValues(optimized.b1, optimized.k)
-);
-
-// console.log(optimized.steps);
-
-createCSV(
-  'data/final.csv',
-  getFunctionValues(0.973, 3.984)
-);
-
-createCSV(
-  'data/rnd.csv',
-  checkRandomDistribution(), 'intrval, occurences'
-);
-
-function getFunctionValues(b1 = 1, k = 4) {
-  const XT = 5, A = 3, B2 = 2, B1 = b1, K = k;
-  let y = 0, z1 = 0, z2 = 0, z3 = 0, h = 0.05, theorValues = [];
-
-  for (let iterations = 1; iterations <= 500; iterations++) {
-    z3 = (XT - z1 - (B1 + A) * z2 - (B1 + A * b1) * z3) / (A * B2);
-    z2 = z2 + h * z3;
-    z1 = z1 + h * z2;
-
-    y = K * (z1 - A * z2);
-
-    if (iterations % 20 == 0) {
-      theorValues.push(y);
+      if (i % 20 == 0) {
+        theorValues.push(y);
+      }
     }
+
+    return theorValues;
   }
 
-  return theorValues;
-}
+  function getExperimental(theorValues, factor) {
+    const deltaY = Math.max.apply(null, theorValues) * factor;
 
-function getExperimental(theorValues, factor) {
-  const deltaY = Math.max.apply(null, theorValues) * factor;
-  let experimentalValues = theorValues.slice();
-
-  for (let i = 0; i < experimentalValues.length; ++i) {
-    experimentalValues[i] += Math.random() * (2 * deltaY) - deltaY;
-  }
-
-  return experimentalValues;
-}
-
-function checkRandomDistribution() {
-  const interval = 0.1;
-  let randomNumber, distribution = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-  for (var i = 0; i < 10000; ++i) {
-    randomNumber = Math.random();
-    ++distribution[Math.floor(randomNumber / interval)];
-  }
-
-  return distribution;
-}
-
-function optimize(callback, startX, startY) {
-  let
-    x = startX, y = startY,
-
-    f = callback(x, y),
-    f1,
-    f2 = f,
-
-    h1 = 0.1,
-    h2 = 0.1,
-    steps = [],
-
-    m = 1;
-
-  while (true) {
-    if (m == 1) {
-      x = x + h1;
-      f1 = callback(x, y);
-
-      if (f1 <= f) {
-          h1 = 3 * h1;
-          f = f1;
-      } else {
-          x = x - h1;
-          h1 = -0.5 * h1;
+    return theorValues.map(function(value) {
+        return value += Math.random() * (2 * deltaY) - deltaY;
       }
+    );
+  }
 
-      ++m;
-    } else if (m != 1) {
-      y = y + h2;
-      f1 = callback(x, y);
+  function checkRandomDistribution() {
+    const interval = 0.1;
+    let randomNumber, distribution = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-      if (f1 <= f) {
-          h2 = 3 * h2;
-          f = f1;
-      } else {
-          y = y - h2;
-          h2 = -0.5 * h2;
-      }
+    for (var i = 0; i < 10000; ++i) {
+      randomNumber = Math.random();
+      ++distribution[Math.floor(randomNumber / interval)];
+    }
+
+    return distribution;
+  }
+
+  function optimize(func, startX, startY, callback) {
+    let
+      x = startX, y = startY,
+
+      f = func(x, y),
+      f1,
+      f2 = f,
+
+      deltaX = 0.1,
+      deltaY = 0.1,
+      steps = [],
 
       m = 1;
-    }
 
-    if (Math.abs(f2 - f1) < 1e-5) {
-      return {x, y, steps};
-    } else {
-      steps.push(`${x}, ${y}, ${f1}`);
-    }
+    while (true) {
+      if (m == 1) {
+        x += deltaX;
+        f1 = func(x, y);
 
-    f2 = f1;
+        if (f1 <= f) {
+            deltaX *= 3;
+            f = f1;
+        } else {
+            x -= deltaX;
+            deltaX *= -0.5;
+        }
+
+        ++m;
+      } else if (m != 1) {
+        y += deltaY;
+        f1 = func(x, y);
+
+        if (f1 <= f) {
+            deltaY *= 3;
+            f = f1;
+        } else {
+            y -= deltaY;
+            deltaY *= -0.5;
+        }
+
+        m = 1;
+      }
+
+      if (Math.abs(f2 - f1) < 1e-5) {
+        callback(x, y, steps);
+
+        return {x, y, steps};
+      } else {
+        steps.push(`${x}, ${y}, ${f1}`);
+      }
+
+      f2 = f1;
+    }
   }
-}
 
-function createCSV(filename, data, headers = 'x, y') {
-  let output = `${headers}\n`;
+  function createCSV(filename, data, headers = 'x, y') {
+    let output = `${headers}\n`;
 
-  for (let i = 0; i < data.length; ++i) {
-    output += `${i + 1}, ${data[i]}\n`;
-  }
-
-  fs.writeFile(filename, output, (err) => {
-    if (err) {
-      return console.error(err);
+    for (let i = 0; i < data.length; ++i) {
+      output += `${i + 1}, ${data[i]}\n`;
     }
 
-    console.info(`${filename} done!`);
-  });
-}
+    fs.writeFile(filename, output, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      console.info(`${filename} done!`);
+    });
+  }
+})();
